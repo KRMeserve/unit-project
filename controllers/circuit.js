4b636cb2325c094dd15a0a84c31ebd8d41c20842
Kyle Meserve <KyleMeserve@Kyles-MacBook-Air.local>
2018-10-26 09:55:31 -0400

    user controller made in circuit.js - need to rename

--- controllers/circuit.js
+++ controllers/circuit.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/users.js');
 
 // SERVER ROUTES WILL GO HERE
 router.get('/', (req, res)=>{
-    res.json('INFORMATION');
+    User.find({}, (error, allUsers)=>{
+        res.json(allUsers);
+    });
+});
+
+router.post('/', (req, res)=>{
+    User.create(req.body, (error, createdUser)=>{
+        res.json(createdUser);
+    });
+});
+
+router.delete('/:id', (req, res)=>{
+    User.findByIdAndRemove(req.params.id, (error, deletedUser)=>{
+        res.json(deletedUser);
+    });
+});
+
+router.put('/:id', (req, res)=>{
+    User.findByIdAndUpdate(req.params.id, req.body, {new: true}, (error, updatedUser)=>{
+        res.json(updatedUser);
+    });
 });
 
 module.exports = router;
