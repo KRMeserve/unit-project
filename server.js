90eff4d62405eb663b685c03181685cc8c1ccb05
Kyle Meserve <KyleMeserve@Kyles-MacBook-Air.local>
2018-10-26 13:41:51 -0400

    dotenv is working and sessions middleware is up

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const session = require('express-session');
 const PORT = process.env.PORT || 3000;
 
 const MONGODB_URI =
   process.env.MONGODB_URL || "mongodb://localhost/" + "circuit";
 
+// Middleware
 app.use(express.json());
 app.use(express.static("public"));
+// Session Middleware
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
 
 const userController = require("./controllers/users.js");
 app.use("/users", userController);
