89fa82b8014e035733587435cba7637136654939
Kyle Meserve <KyleMeserve@Kyles-MacBook-Air.local>
2018-10-26 10:02:16 -0400

    get route is working in main controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,30 +1,35 @@
+// -------- REQUIRED CONSTANTS --------
 const express = require('express');
 const router = express.Router();
 const User = require('../models/users.js');
 
-// SERVER ROUTES WILL GO HERE
+// -------- GET ROUTE FOR USERS --------
 router.get('/', (req, res)=>{
     User.find({}, (error, allUsers)=>{
         res.json(allUsers);
     });
 });
 
+// -------- CREATE ROUTE FOR USERS --------
 router.post('/', (req, res)=>{
     User.create(req.body, (error, createdUser)=>{
         res.json(createdUser);
     });
 });
 
+// -------- DELETE ROUTE FOR USERS --------
 router.delete('/:id', (req, res)=>{
     User.findByIdAndRemove(req.params.id, (error, deletedUser)=>{
         res.json(deletedUser);
     });
 });
 
+// -------- UPDATE ROUTE FOR USERS --------
 router.put('/:id', (req, res)=>{
     User.findByIdAndUpdate(req.params.id, req.body, {new: true}, (error, updatedUser)=>{
         res.json(updatedUser);
     });
 });
 
+// -------- EXPORT USERS --------
 module.exports = router;
--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,21 @@
+// -------- REQUIRED CONSTANTS FOR CIRCUIT ENTIRE APP --------
 const app = angular.module('CircuitApp', []);
 
+// -------- MAIN CONTROLLER --------
 app.controller('MainController', ['$http', function($http){
+    // -------- Test route --------
     this.hello = 'Howdy';
-}])
+
+    // -------- Get users route --------
+    this.getUsers = ()=>{
+        $http({
+            method: 'GET',
+            url: '/users'
+        }).then(response=>{
+            console.log(response);
+        }, error =>{
+            console.log(error);
+        })
+    };
+    this.getUsers();
+}]);
